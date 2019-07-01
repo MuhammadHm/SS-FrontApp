a6e4a18462ef25518c594a582f9c1893b3e3d208
MuhammadHm <mohamad.1999.syr@gmail.com>
2019-07-02 01:29:12 +0300

    hm

--- src/Side Bar/Sidebar.js
+++ src/Side Bar/Sidebar.js
@@ -15,7 +15,7 @@ class Sidebar extends Component {
                     <a className="s-logo" href={backHome}>Survey<span>Space</span></a>
                 </h1>
                 <ul>
-                    <li><a href="/createsurvey">Design Survey</a></li>
+                    <li><a href="/createsurvey">Create Survey</a></li>
                     <li><a href={preview} >Preview &amp; Print</a></li>
                     <li><a href="/publish">Publish Survey</a></li>
                     <li><a href={responses}>Collect Responses</a></li>
