5397a2ffd2524d549f206cb1c17d4a6bebe4ff64
MuhammadHm <mohamad.1999.syr@gmail.com>
2019-07-02 20:54:29 +0300

    hm

--- src/Publish/publish.js
+++ src/Publish/publish.js
@@ -1,5 +1,19 @@
 import React, { Component } from 'react';
 import './publish.css'
+import {
+    FacebookShareButton,
+    LinkedinShareButton,
+    TwitterShareButton,
+    WhatsappShareButton,
+    EmailShareButton,
+  } from 'react-share';
+  import {
+    FacebookIcon,
+    TwitterIcon,
+    WhatsappIcon,
+    LinkedinIcon,
+    EmailIcon,
+  } from 'react-share';
 
 class Publish extends Component {
     
@@ -10,51 +24,27 @@ class Publish extends Component {
             survey_id :'',
         }
     }
-
-
-/*
-    componentDidMount(){
-    
-        fetch(`http://localhost:8080/survey/publish`)
-        .then(response =>  response.json())
-          .then(data => {
-            this.setState({
-                survey_id : data.survey_id,
-                           
-            });
-            console.log("data",data);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-        
-    }*/
-    
-    
     
     render() {
-        
-        let style={
-            "fontSize" : "35px",
-            "color" : "#383f50",
-            "width" : "600px",
-            "padding" : "20px",
-            "marginLeft" : "25%",
-            "marginTop" : "2%",
-            "textAlign" : "left"
-        };
 
         let link=`http://localhost:3000/userpreview/${this.props.survey_id}`;
 
         return (
             <div className="textbox">
-                <label style={style} >Share your survey via this web link : </label>
+                <label className="publish" >Share your survey via this web link : </label>
                 <br />
-                <input className="share" name="text" type="text" value={link} style={style} />
+                <input className="publish" name="text" type="text" value={link} className="publish" />
+                <br />
+                <label className="publish" >Or Share via social media : </label>   
+                <FacebookShareButton  className="icon" url={link} children={FacebookIcon} ><FacebookIcon size={100}  round={true} /></FacebookShareButton>
+                <WhatsappShareButton  className="icon" url={link} children={WhatsappIcon} ><WhatsappIcon size={100}  round={true} /></WhatsappShareButton>
+                <TwitterShareButton  className="icon" url={link} children={FacebookIcon} ><TwitterIcon size={100}  round={true} /></TwitterShareButton>
+                <EmailShareButton   className="icon" url={link} children={EmailIcon} ><EmailIcon size={100}  round={true} /></EmailShareButton>
+                <LinkedinShareButton  className="icon" url={link} children={FacebookIcon} ><LinkedinIcon size={100}  round={true} /></LinkedinShareButton>
+               
             </div>
         );
     }
-
 }
 export default Publish;
 
